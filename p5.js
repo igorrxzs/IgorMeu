e198d50c8a7419e1b6c5fed0790dbb825e968b13
Igor Alves <igoralves5928@gmail.com>
2026-03-03 20:33:55 -0300

    Add files via upload

---
+++ p5.js
@@ -0,0 +1,61 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  circle (200,200,250,250);
+  circle (200, 200, 150, 150);
+  circle (200, 200, 80, 80);
+  line (200, 125, 200, 275);
+  line (75, 200, 325, 200);  
+  
+}
+
+function draw() {
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  rect (150, 150, 100, 100);
+  line (200, 150, 200, 250);
+  line (150, 200, 250, 200); 
+}
+
+function draw() {
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  circle (200,200,250,250);
+  circle (150, 150, 40, 70);
+  circle (250, 150, 40, 70);
+  line (150, 230, 260, 230);
+}
+
+function draw() {
+  
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  circle (200,120,140, 150);
+  circle (170, 100, 20, 60);
+  circle (230, 100, 20, 60);
+  circle (200, 120, 10, 60);
+  line (160, 140, 240, 140);
+  circle (200, 258, 180, 150);
+  circle (200, 370, 200, 180);
+  line (249, 219, 350, 200);  
+  line (165, 196, 65, 168);
+  circle (200, 190, 20, 150);
+  circle (200, 250, 20, 150);
+  circle (200, 350, 20, 150);
+  circle (200, 300, 20, 150);
+}
+function draw() {
+  
+}
