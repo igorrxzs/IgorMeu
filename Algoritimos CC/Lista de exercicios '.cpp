c803bb5c8557b5b002b60e426b6efa9436444996
Igor Alves <igoralves5928@gmail.com>
2026-03-03 11:34:23 -0300

    Add files via upload

---
+++ Algoritimos CC/Lista de exercicios '.cpp
@@ -0,0 +1,32 @@
+/* #include <stdio.h>
+
+int main() {
+    printf("Bom dia! Estou aprendendo a programar Definitivamente nao eh \"muito\" dificil");
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    printf("Suas notas sao 7 e 9 e sua media eh %d", (7 + 9) / 2);
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    printf("Vamos distribuir 359 carros para 4 pessoas e portanto devem sobrar %d carros", 359 % 4);
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    printf("Eu tenho um circulo de raio 5. Portanto o diametro eh %d, a area eh %.2f e a medida da circunferencia eh %.2f", 
+           2 * 5, 
+           3.14 * 5 * 5, 
+           2 * 3.14 * 5);
+    return 0;
+}
+
+*/
---
+++ Algoritimos CC/Lista de exercicos 2.cpp
@@ -0,0 +1,130 @@
+/* 
+#include <stdio.h>
+
+int main() {
+    float nota1, nota2, media;
+    printf("Digite a nota da primeira prova: ");
+    scanf("%f", &nota1);
+    printf("Digite a nota da segunda prova: ");
+    scanf("%f", &nota2);
+    media = (nota1+nota2)/2;
+    printf("A media das notas é: %.2f", media);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float consumomedio, distancia, combustivel;
+    printf("Informe a distancia total percorrida: ");
+    scanf("%f", &distancia);
+    printf("Informe o combustivel consumido: ");
+    scanf("%f", &combustivel);
+    consumomedio = distancia / combustivel;
+    printf("O consumo medio foi de: %.2f", consumomedio);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int homens, mulheres;
+    float total;
+    printf("Quantos homens participaram da festa?: ");
+    scanf("%d", &homens);
+    printf("Quantos mulheres participaram da festa?: ");
+    scanf("%d", &mulheres);
+    total = (homens * 20) + (mulheres * 17);
+    printf("O valor total arrecadado foi de: %.2f", total);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float raio, altura, volume;
+    float PI;
+    PI = 3.14;
+    printf("Qual o raio do cilindro?: ");
+    scanf("%f", &raio);
+    printf("Qual a altura do cilindro?: ");
+    scanf("%f", &altura);
+    volume = PI * raio*raio * altura;
+    printf("O volume do seu cilindro é: %.2f", volume);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float A, B, C, delta;
+    printf("Digite o valor de A: ");
+    scanf("%f", &A);
+    printf("Digite o valor de B: ");
+    scanf("%f", &B);
+    printf("Digite o valor de C: ");
+    scanf("%f", &C);
+    delta = B*B - 4 * A * C;
+    printf("O valor de delta é: %.2f", delta);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int numero, resultado;
+    printf("Informe um numero: ");
+    scanf("%d", &numero);
+    resultado = numero % 2;
+    printf("O resultado é: %d", resultado);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float grausc, grausf, resultado;
+    printf("Digite a temparatura em Celcius: ");
+    scanf("%f", &grausc);
+    resultado = (grausc * 9/5) + 32;
+    printf("A temperatura em graus Fahrenheit é: %.2f", resultado);
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    float num1, num2, num3, media;
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%f", &num2);
+    printf("Digite o terceiro numero: ");
+    scanf("%f", &num3);
+    media = (num1 + num2 + num3) / 3;
+    printf("A media aritmetica dos numeros, é: %.2f", media);
+    
+    return 0;
+}
+*/
