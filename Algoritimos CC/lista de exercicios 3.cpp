f0551fb2f3d979e27b521d86d684b7ad4e32331a
Igor Alves <igoralves5928@gmail.com>
2026-03-09 23:27:48 -0300

    Add files via upload

---
+++ Algoritimos CC/lista de exercicios 3.cpp
@@ -0,0 +1,116 @@
+/* #include <stdio.h>
+
+int main()
+{
+    int numero;
+    printf("Informe um numero: ");
+    scanf("%d", &numero);
+    if (numero >= 10) {
+        printf("Sim!");
+    }
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Informe um número: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("Esse %d é par", num);
+    }
+    else {
+        printf("Esse %d é impar", num);
+    }
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Informe o segundo numero: ");
+    scanf("%d", &num2);
+    if (num1 > num2) {
+        printf("%d é maior que %d", num1, num2);
+    }
+    else {
+        printf("%d é maior que %d", num2, num1);
+    }
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Informe o segundo numero: ");
+    scanf("%d", &num2);
+    if (num1 > num2) {
+        printf("%d é maior que %d", num1, num2);
+    }
+    if (num2 > num1) {
+        printf("%d e maior que %d", num2, num1);
+    }
+    else {
+        printf("Os numeros são iguais.");
+    }
+    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    printf("Informe um número: ");
+    scanf("%d", &numero);
+    if (numero % 10 == 0) {
+        printf("%d é multiplo de 10.", numero);
+    }
+    else {
+        printf("%d não é multiplo de 10.", numero);
+    }    
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+int main ()
+{
+    int num1, num2;
+    printf("Informe o primeiro número: ");
+    scanf("%d", &num1);
+    printf("Informe o segundo número: ");
+    scanf("%d", &num2);
+    if (num1 >= 10 && num2) {
+        printf("Sim.");
+    }
+    else {
+        printf("Ambos não são maior que 10.");
+    }
+    
+    return 0;
+}
+*/
